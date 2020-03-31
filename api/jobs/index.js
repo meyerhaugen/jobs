e79104f1e5b6fc18840e0a40e5d491dd6c707b7a
Espen Th Sæverud <espensaeverud@gmail.com>
2020-03-31 11:06:46 +0200

    feat: add endpoint

---
+++ api/jobs/index.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+const instance = axios.create()
+
+const errorHandler = error => {
+  throw error
+}
+
+const successHandler = response => {
+  return response.data
+}
+
+instance.interceptors.response.use(successHandler, errorHandler)
+
+const url = "https://api.recman.no/v2/get/?key=200323034950k1efe349a3709948943fd306abbaf87401580025698&scope=jobPost&fields=name,ingress,startDate,endDate"
+
+export default async (req, res) => {
+  const result = await instance.get(url)
+  const myData = {
+    ...result,
+    jobs: Object.keys(result.data).map(key => result.data[key])
+  }
+  res.status(200).send(myData)
+}
